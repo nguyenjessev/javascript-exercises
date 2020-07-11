daf23438742c42448e645371fd5baa7f07c1dc66
Jesse <nguyenjessev@gmail.com>
2020-07-10 20:34:57 -0400

    Implement removeFromArray function

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...args) {
+    args.forEach((key) => {
+        if (myArray.indexOf(key) >= 0) {
+            myArray.splice(myArray.indexOf(key), 1);
+        }
+    })
 
+    return myArray;
 }
 
 module.exports = removeFromArray
