62ddf52c7f1cdbc6bdd9048339c0b0ac341a6dde
Jesse <nguyenjessev@gmail.com>
2020-07-10 21:01:41 -0400

    GAUSS BIATCH

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof(a) != "number"
+        || typeof(b) != "number"
+        || a < 0
+        || b < 0) {
+        return "ERROR";
+    }
 
+    return (a+b) * (Math.abs(a-b) + 1) / 2;
 }
 
 module.exports = sumAll
