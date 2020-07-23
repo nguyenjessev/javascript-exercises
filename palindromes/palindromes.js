306e62083e424746204cffdff91a65030e781df2
Jesse <nguyenjessev@gmail.com>
2020-07-22 23:51:52 -0400

    Implement palindromes()

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function() {
+const palindromes = function(s) {
+    s = s.toLowerCase()
+         .replace(' ', '');
+    const reversed = s.split('')
+                .reverse()
+                .join('');
 
+    return s === reversed;
 }
 
 module.exports = palindromes
