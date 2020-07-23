8c7836bc6d394d6ca8b88f1b20cc10d4d1e177b4
Jesse <nguyenjessev@gmail.com>
2020-07-22 23:41:31 -0400

    Implement factorial()

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -18,8 +18,12 @@ function power(a, b) {
 	return Math.pow(a, b);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	let total = 1;
+	for (let i = 1; i <= num; i++) {
+		total *= i;
+	}
+	return total;
 }
 
 module.exports = {
