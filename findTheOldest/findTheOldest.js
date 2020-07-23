58798e2a6080998544e25859761a054b1ffa45c0
Jesse <nguyenjessev@gmail.com>
2020-07-23 19:17:39 -0400

    Ayy

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    people.sort((a, b) => getAge(b) - getAge(a));
 
+    return people[0];
+}
+
+function getAge(person) {
+    if (person.yearOfDeath) return person.yearOfDeath - person.yearOfBirth;
+
+    return new Date().getFullYear() - person.yearOfBirth;
 }
 
 module.exports = findTheOldest
