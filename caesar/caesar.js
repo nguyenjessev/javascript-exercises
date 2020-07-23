bf6b6023be1c716c0bfede8ae64db6ee50d5959f
Jesse <nguyenjessev@gmail.com>
2020-07-23 18:54:12 -0400

    Kill me now

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,9 +1,31 @@
 const caesar = function(s, offset) {
-    const result = s.split('')
-                    .map(c => String.fromCharCode( c.charCodeAt(0) + offset ))
-                    .join(''); 
+    let chars = s.split('');
+    chars = chars.map(char => {
+        const charCode = char.charCodeAt();
 
-    return result;
+        if (charCode < 65 || charCode > 122 || (charCode > 90 && charCode < 97)) {
+            return char;
+        }
+
+        let newCharCode = charCode + offset;
+        const startCode = getStart(charCode);
+
+        newCharCode -= startCode;
+        newCharCode %= 26;
+        newCharCode += 26;
+        newCharCode %= 26;
+        newCharCode += startCode;
+
+        return String.fromCharCode(newCharCode);
+    });
+
+    return chars.join('');
+}
+
+function getStart(code) {
+    return code < 97 ? 65 : 97;
 }
 
 module.exports = caesar
+
+// Find distance from start of alphabet
