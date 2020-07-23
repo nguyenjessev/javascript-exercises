36b2733d1a37457003429b0aadbe02dfadc1acd7
Jesse <nguyenjessev@gmail.com>
2020-07-23 00:21:37 -0400

    Implement caesar()

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,9 @@
-const caesar = function() {
+const caesar = function(s, offset) {
+    const result = s.split('')
+                    .map(c => String.fromCharCode( c.charCodeAt(0) + offset ))
+                    .join(''); 
 
+    return result;
 }
 
 module.exports = caesar
